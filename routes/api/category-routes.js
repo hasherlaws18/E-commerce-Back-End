5f67f2aa3b2457319c1ea3b04bb241f986375c2f
hasherlaws18 <houstonasherlaws@gmail.com>
2023-04-26 16:01:13 -0500

    new changes to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,72 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{
+    const CategoryData = await Category.findAll();
+    res.status(200).json(CategoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product, as: 'product'}]
+    });
+
+    if(!CategoryData) {
+      res.status(400).json({message: 'No location found with this id!'});
+      return;
+    }
+    res.status(200).json(CategoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
+    const CategoryData = await Category.create(req.body);
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try{
+    const CategoryData = await Category.Update(req.body);
+
+    if(!CategoryData) {
+      res.status.json(CategoryData);
+   }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try{
+    const CategoryData = await Category.destroy({
+      where:{id: req.params.id}
+    });
+    
+    if(!CategoryData){
+      res.status.json(CategoryData);
+    } 
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete a category by its `id` value
 });
 
